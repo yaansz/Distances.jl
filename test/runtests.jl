b0b6c01b74dce5a61c5bba5bc07df632d5b5d669
Yan Figueiredo <yancfm@gmail.com>
2020-07-02 21:46:22 -0300

    Tests - v1

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using Distances
 using Test
 
 @testset "Distances tests" begin
-    @test GetDistance(Euclidean(), [2,2], [2,4]) == 2
-    @test GetDistance(CityBlock::DistanceType, [2,2], [2,4]) == 2
+    @test GetDistance(Euclidean::DistanceType, [2,2], [2,4]) == 2
+    @test GetDistance(Cityblock::DistanceType, [2,2], [2,4]) == 2
     @test GetDistance(TotalVariation::DistanceType, [2,2], [2,4]) == 1
     @test GetDistance(Chebyshev::DistanceType, [2,2], [2,4]) == 2
     @test GetDistance(Jaccard::DistanceType, [2,2], [2,4]) == 1 - 2/3
